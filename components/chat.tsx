5c2b9461a50f6d7bc6ac781c2e6c10d74411ea19
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2024-12-10 17:20:31 +0700

    add multimodal input component

--- components/chat.tsx
+++ components/chat.tsx
@@ -3,11 +3,31 @@
 import { ChatHeader } from "@/components/chat-header";
 import { MultimodalInput } from "./multimodal-input";
 
+import { useScrollToBottom } from "@/components/use-scroll-to-bottom";
+
 export function Chat() {
+  const [messagesContainerRef, messagesEndRef] =
+    useScrollToBottom<HTMLDivElement>();
+
   return (
     <>
       <div className="flex flex-col min-w-0 h-dvh bg-background">
         <ChatHeader />
+
+        <div
+          ref={messagesContainerRef}
+          className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
+        >
+          {/* <PreviewMessage />
+
+          <ThinkingMessage /> */}
+
+          <div
+            ref={messagesEndRef}
+            className="shrink-0 min-w-[24px] min-h-[24px]"
+          />
+        </div>
+
         <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
           <MultimodalInput />
         </form>
--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -1,5 +1,48 @@
 "use client";
 
+import type React from "react";
+import { useRef } from "react";
+
+import { ArrowUp, Paperclip } from "lucide-react";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+
 export function MultimodalInput() {
-  return <div className="relative w-full flex flex-col gap-4">not yet</div>;
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  return (
+    <div className="relative w-full flex flex-col gap-4">
+      <Textarea
+        ref={textareaRef}
+        placeholder="Send a message..."
+        className="min-h-[24px] max-h-[calc(75dvh)] overflow-hidden resize-none rounded-xl text-base bg-muted"
+        rows={3}
+        autoFocus
+        onKeyDown={(event) => {
+          if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault();
+          }
+        }}
+      />
+
+      <Button
+        className="rounded-full p-1.5 h-fit absolute bottom-2 right-2 m-0.5 border dark:border-zinc-600"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <ArrowUp size={14} />
+      </Button>
+
+      <Button
+        className="rounded-full p-1.5 h-fit absolute bottom-2 right-11 m-0.5 dark:border-zinc-700"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        variant="outline"
+      >
+        <Paperclip size={14} />
+      </Button>
+    </div>
+  );
 }
