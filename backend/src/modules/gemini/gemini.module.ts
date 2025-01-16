2f4f595b0eec161102bc26b6887f11f132e6fa2f
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2025-01-16 19:14:58 +0700

    remove unused files

--- backend/src/modules/gemini/gemini.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GeminiService } from './gemini.service';
-import { GeminiController } from './gemini.controller';
-
-@Module({
-  providers: [GeminiService],
-  controllers: [GeminiController],
-})
-export class GeminiModule {}
