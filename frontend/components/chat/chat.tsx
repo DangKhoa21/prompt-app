052d70fea41d52f8a4ad92146812604d5f9220e4
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2025-02-08 21:13:22 +0700

    fix

--- frontend/components/chat/chat.tsx
+++ frontend/components/chat/chat.tsx
@@ -11,7 +11,7 @@ import { MultimodalInput } from "../multimodal-input";
 
 import { useScrollToBottom } from "@/components/use-scroll-to-bottom";
 import { Overview } from "@/components/overview";
-import { SERVER_URL } from "@/config";
+import { SERVER_URL, VERSION_PREFIX } from "@/config";
 import { useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
@@ -41,7 +41,7 @@ export function Chat({
     stop,
     // data: streamingData,
   } = useChat({
-    api: `${SERVER_URL}/api/v1/chat`,
+    api: `${SERVER_URL}/${VERSION_PREFIX}/chat`,
     id,
     body: { id, modelId: selectedModelId },
     initialMessages,
--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,2 +1,4 @@
-export const SERVER_URL = process.env.SERVER_URL ?? "http://localhost:3001";
-export const VERSION_PREFIX = process.env.VERSION_PREFIX ?? "api/v1";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:3001";
+export const VERSION_PREFIX =
+  process.env.NEXT_PUBLIC_VERSION_PREFIX ?? "api/v1";
