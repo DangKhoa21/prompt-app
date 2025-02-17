82364aaf346930aa0a0964018a6cf4816a30b67c
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2025-02-18 02:19:15 +0700

    optimized marketplace page

--- frontend/app/(home)/marketplace/page.tsx
+++ frontend/app/(home)/marketplace/page.tsx
@@ -1,77 +1,19 @@
-"use client";
+import React, { Suspense } from "react";
 
-import React from "react";
-
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { LoadingSpinner } from "@/components/icons";
 import { Search } from "lucide-react";
 
-import { getPrompts, getTags } from "@/services/prompt";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { useInView } from "react-intersection-observer";
-import PromptHoverCard from "@/components/prompt/prompt-hover-card";
-import { useSearchParams } from "next/navigation";
+import TagsList from "@/components/marketplace/tagslist";
+import PromptsList from "@/components/marketplace/promptslist";
 
-export default function Page() {
-  const searchParams = useSearchParams();
-  const tagId = searchParams.get("tagId");
-  const { ref, inView } = useInView();
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery({
-    queryKey: ["prompts", tagId],
-    queryFn: ({ pageParam }) => getPrompts({ pageParam, tagId }),
-    initialPageParam: "",
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-  });
-
-  React.useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [fetchNextPage, hasNextPage, inView]);
-
-  const updateTagId = React.useCallback(
-    (tagId: string) => {
-      const params = new URLSearchParams(searchParams.toString());
-      if (tagId.length) params.set("tagId", tagId);
-      else params.delete("tagId");
-      window.history.pushState(null, "", `?${params.toString()}`);
-    },
-    [searchParams]
-  );
-
-  const {
-    isPending: isTagsLoading,
-    isError: isTagsError,
-    data: tagsData,
-    error: tagsError,
-  } = useQuery({
-    queryKey: ["tags"],
-    queryFn: () => getTags(),
-  });
-
-  if (status === "pending") {
-    return null;
-  }
-
-  if (status === "error") {
-    return <span>Error: {error.message}</span>;
-  }
-
-  if (isTagsLoading) {
-    return null;
-  }
-
-  if (isTagsError) {
-    return <span>Error: {tagsError.message}</span>;
-  }
+export default async function Page(props: {
+  searchParams?: Promise<{
+    tagId?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const tagId = searchParams?.tagId || "";
 
   return (
     <div className="flex-1 overflow-auto bg-background">
@@ -90,37 +32,13 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="flex flex-wrap gap-3 mb-8 justify-center">
-          {tagsData.map((tag) => (
-            <Button
-              key={tag.id}
-              variant={tag.id === tagId ? "secondary" : "outline"}
-              size="sm"
-              className="rounded-2xl gap-1 px-4"
-              onClick={() =>
-                tag.id === tagId ? updateTagId("") : updateTagId(tag.id)
-              }
-            >
-              {tag.name}
-            </Button>
-          ))}
-        </div>
+        <Suspense fallback={<LoadingSpinner />}>
+          <TagsList />
+        </Suspense>
 
-        <div className="px-0 py-8 md:px-4 bg-background-primary grid gap-6 justify-evenly justify-items-center grid-cols-[repeat(auto-fit,_280px)]">
-          {data.pages.map((group, i) => (
-            <React.Fragment key={i}>
-              {group.data.map((prompt) => (
-                <PromptHoverCard key={prompt.id} {...prompt} />
-              ))}
-            </React.Fragment>
-          ))}
-        </div>
-
-        <div className="flex justify-center">
-          {isFetchingNextPage ? <LoadingSpinner /> : null}
-        </div>
-
-        <div ref={ref} className="mt-5" />
+        <Suspense fallback={<LoadingSpinner />}>
+          <PromptsList tagId={tagId} />
+        </Suspense>
       </div>
     </div>
   );
